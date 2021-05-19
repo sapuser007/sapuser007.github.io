c080d1f03c294ceb4b6b8320344960789266ca99
sapuser007 <84439619+sapuser007@users.noreply.github.com>
2021-05-19 18:13:38 +0530

    Update App.controller.js

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -6,30 +6,7 @@ sap.ui.define([
 
 	return BaseController.extend("LMES.CovidTracker2021.controller.App", {
 
-// 		onInit : function () {
-// 			var oViewModel,
-// 				fnSetAppNotBusy,
-// 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 
-// 			oViewModel = new JSONModel({
-// 				busy : true,
-// 				delay : 0
-// 			});
-// 			this.setModel(oViewModel, "appView");
-
-// 			fnSetAppNotBusy = function() {
-// 				oViewModel.setProperty("/busy", false);
-// 				oViewModel.setProperty("/delay", iOriginalBusyDelay);
-// 			};
-
-			// disable busy indication when the metadata is loaded and in case of errors
-// 			this.getOwnerComponent().getModel().metadataLoaded().
-// 				then(fnSetAppNotBusy);
-// 			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
-
-			// apply content density mode to root view
-// 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-// 		}
 	});
 
 });
