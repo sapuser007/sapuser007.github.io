5d44c2cc798ece6cd171172d8a6ac22d0375bf7d
sapuser007 <84439619+sapuser007@users.noreply.github.com>
2021-05-19 13:43:10 +0530

    Update App.controller.js

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -24,8 +24,8 @@ sap.ui.define([
 
 			// disable busy indication when the metadata is loaded and in case of errors
 // 			this.getOwnerComponent().getModel().metadataLoaded().
-				then(fnSetAppNotBusy);
-			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
+// 				then(fnSetAppNotBusy);
+// 			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
 
 			// apply content density mode to root view
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
