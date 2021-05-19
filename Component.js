e52ece42bda0e6f254288020a8cbfc9aebe48ba0
sapuser007 <84439619+sapuser007@users.noreply.github.com>
2021-05-19 13:52:26 +0530

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-	"./model/models",
+	"./model/models"
 // 	"./controller/ErrorHandler"
 ], function (UIComponent, Device, models) {
 	"use strict";
